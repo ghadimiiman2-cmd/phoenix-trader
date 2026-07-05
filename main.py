e447324dcd6622a05303355439fdc760eca16f3d
ghadimiiman2-cmd <ghadimiiman2@gmail.com>
2026-07-05 05:59:52 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("Phoenix Trader is running...")
+import requests
+
+def get_price(symbol="BTCUSDT"):
+
+    url = "https://api.binance.com/api/v3/ticker/price"
+
+    r = requests.get(url, params={"symbol": symbol})
+
+    return float(r.json()["price"])
+
+price = get_price()
+
+print("BTC Price:", price)
